34326cbe15f3e4f238a69dc188f3a4d5bec63a99
Ernest Gupik <ernestgupik@wp.pl>
2021-03-10 00:10:48 +0100

    Code refactor - make app work as a client instead of a server. (logical mistake)

--- src/irc.h
+++ src/irc.h
@@ -1,28 +1,50 @@
 #ifndef IRC_H
 #define IRC_H
 
+#include <iostream>
+#include <deque>
 #include <boost/asio.hpp>
 
-class Session : public std::enable_shared_from_this<Session> {
-    public:
-        boost::asio::ip::tcp::socket socket_;
-        Session(boost::asio::ip::tcp::socket socket);
+using boost::asio::ip::tcp;
 
-    void start();
+class chat_message {
+    public:
+        enum { header_length = 4 };
+        enum { max_body_length = 512 };
+
+        chat_message();                                       // done
+        const char* data() const;                             // done
+        char* data();                                         // done
+        std::size_t length() const;                           // done
+        const char* body() const;                             // done
+        char* body();                                         // done
+        std::size_t body_length() const;                      // done
+        void body_length(std::size_t new_length);             // done
+        bool decode_header();                                 // done
+        void encode_header();                                 // done
 
     private:
-        void do_read();
-
-    void do_write(std::size_t length);
+        char data_[header_length + max_body_length];
+        std::size_t body_length_;
 };
 
-class Server {
-    public:
-        boost::asio::ip::tcp::acceptor acceptor_;
-        Server(boost::asio::io_context& io_context, short port);
+typedef std::deque<chat_message> chat_message_queue;
 
+class chat_client {
+    public:
+        chat_client(boost::asio::io_context& io_context, const tcp::resolver::results_type& endpoints);                                        // done
+        void write(const chat_message& msg);                  // done
+        void close();                                         // done
+    
     private:
-        void do_accept();
+        void do_connect(const tcp::resolver::results_type& endpoints);                                    // done
+        void do_read_header();                                // done
+        void do_read_body();                                  // done
+        void do_write();                                      // done
+        boost::asio::io_context& io_context_;
+        tcp::socket socket_;
+        chat_message read_msg_;
+        chat_message_queue write_msgs_;
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,37 @@
 #include <iostream>
+#include <deque>
 #include <boost/asio.hpp>
 #include "irc.h"
 
+using boost::asio::ip::tcp;
+
 int main(int argc, char* argv[]) {
     try {
-        if (argc != 2) {
-            std::cerr << "Usage: " << argv[0] << " <port>\n";
+        if(argc!=3) {
+            std::cerr << "Usage: " << argv[0] << " <host> <port>\n";
             return 1;
         }
-
         boost::asio::io_context io_context;
 
-        Server s(io_context, std::atoi(argv[1]));
+        boost::asio::ip::tcp::resolver resolver(io_context);
+        tcp::resolver::results_type endpoints = resolver.resolve(argv[1], argv[2]);
+        chat_client c(io_context, endpoints);
+        std::thread t([&io_context](){ io_context.run(); });
+        char line[chat_message::max_body_length + 1];
 
-        io_context.run();
-    }
-    catch (std::exception& e) {
-        std::cerr << "Exception: " << e.what() << "\n";
+        while (std::cin.getline(line, chat_message::max_body_length + 1)) {
+            chat_message msg;
+            msg.body_length(std::strlen(line));
+            std::memcpy(msg.body(), line, msg.body_length());
+            msg.encode_header();
+            c.write(msg);
+        }
+
+        c.close();
+        t.join();
+
+    } catch(std::exception& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
     }
 
     return 0;
